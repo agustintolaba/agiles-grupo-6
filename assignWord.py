21a28a4fece8f2779f8283dacc20fc1e02993274
facundo Lopez <facundolopez@gmail.com>
2024-09-17 20:13:19 -0300

    Test ingresa palabra manual

--- assignWord.py
+++ assignWord.py
@@ -1,11 +1,11 @@
 import random 
 
 class Ahorcado: 
-  def __init__(self, palabras):
-    self.palabras=palabras
+  def __init__(self):
     self.palabra_secreta=None
 
-  def asignar_palabra(self): 
-    self.palabra_secreta=random.choice(self.palabras)
+  def ingresa_palabra(self,palabra): 
+    self.palabra_secreta= palabra
     return self.palabra_secreta   
   
+
--- test.py
+++ test.py
@@ -2,16 +2,15 @@ import unittest
 from assignWord import Ahorcado 
 
 class TestAhorcado(unittest.TestCase):
-  def setUp(self):
-    self.palabras=['dualidad','sensibilidad','costos','reducidos','exponencial','hipergeometrica']
+#  def setUp(self):
+#   self.palabras=['dualidad','sensibilidad','costos','reducidos','exponencial','hipergeometrica']
 
-  def test_asignar_palabra_aleatoriamente(self):
-    ahorcado=Ahorcado(self.palabras)
-    palabra_asignada=ahorcado.asignar_palabra()
-    #Prueba 
+  def test_asignar_palabra_manualmente(self):
+    ahorcado=Ahorcado()
+    palabra_manual = 'operativa'
+    palabra_asignada= ahorcado.ingresa_palabra(palabra_manual)
 
-    self.assertIn(palabra_asignada,self.palabras)
-
+    self.assertEqual(palabra_asignada,palabra_manual)
 
 
 if __name__=="__main__":
