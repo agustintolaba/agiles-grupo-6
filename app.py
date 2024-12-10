ea7c2eb71f8278c3c6e01ccc7432377ebddcf234
AgustinTolaba <agustin.tolaba.aaa@gmail.com>
2024-12-09 23:04:06 -0300

    hacer deploy 1

--- app.py
+++ app.py
@@ -83,6 +83,10 @@ def reiniciar():
     return render_template("inicio.html")
 
 
+"""if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)"""
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
 
